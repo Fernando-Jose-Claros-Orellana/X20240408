14f4b33c3a9f953fe096aa86074b91a62dfa2259
Fernando Jose Claros Orellana <ferinocente357@gmail.com>
2024-04-08 08:49:01 -0600

    Se termino capa AccesoADatos y La capa LogiaDeNegocio

---
+++ X20240408.LogicaDeNegocio/DependecyContainer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using X20240408.AccesoADatos;
+
+namespace X20240408.LogicaDeNegocio
+{
+    public static class DependecyContainer
+    {
+        public static IServiceCollection AddBLDependecies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDALDependecies(configuration);
+            services.AddScoped<PersonaXBL>();
+            return services;
+        }
+
+    }
+}
---
+++ X20240408.LogicaDeNegocio/PersonaXBL.cs
@@ -0,0 +1,43 @@
+﻿
+
+using X20240408.AccesoADatos;
+using X20240408.EntidadesDeNegocio;
+
+namespace X20240408.LogicaDeNegocio
+{
+    public class PersonaXBL
+    {
+        readonly PersonaXDAL _personaXDAL;
+        public PersonaXBL(PersonaXDAL personaXDAL)
+        {
+            _personaXDAL = personaXDAL;
+        }
+
+        public async Task<int> Crear(PersonaX personax)
+        {
+            return await _personaXDAL.Crear(personax);
+        }
+        public async Task<int> Modificar(PersonaX personaX)
+        {
+            return await _personaXDAL.Modificar(personaX);
+        }
+        public async Task<int> Eliminar(PersonaX personaX)
+        {
+            return await _personaXDAL.Eliminar(personaX);
+        }
+        public async Task<PersonaX> ObtenerPorId(PersonaX personaX)
+        {
+            return await _personaXDAL.ObtenerPorId(personaX);
+        }
+        public async Task<List<PersonaX>> ObtenerTodos()
+        {
+            return await _personaXDAL.ObtenerTodos();
+        }
+        public async Task<List<PersonaX>> Buscar(PersonaX personaX)
+        {
+            return await _personaXDAL.Buscar(personaX);
+        }
+
+
+    }
+}
